7305cb80942a829f6339ae635e078d0b73b6717c
akash-2003-tech <akashsardar378@gmail.com>
2026-09-12 01:11:51 +0530

    feat: implement user registration and login functionality with JWT authentication chore: add  user model

--- Jobs-API/controller/auth.js
+++ Jobs-API/controller/auth.js
@@ -1,5 +1,21 @@
+const User = require('../model/user')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError} =  require('../errors')
+
+
 const register = async(req,res)=>{
-  res.send('Resiter user')
+
+  //Password hashing 
+  // const {name,email,password} = req.body
+  // const salt = await bcrypt.genSalt(10)
+  // const hashedPassword = await bcrypt.hash(password, salt)
+  // const tempUser = {name,email, password: hashedPassword}
+
+  const user = await User.create({...req.body})
+  const token = user.createJWT()
+  res
+    .status(StatusCodes.CREATED)
+    .json({user: {name: user.name}, token})
 }
 
 const login = async(req,res)=>{
--- Jobs-API/middleware/error-handler.js
+++ Jobs-API/middleware/error-handler.js
@@ -4,7 +4,7 @@ const {StatusCodes} = require('http-status-codes')
 
 const errorHandlerMiddleware = (err,req,res,next)=>{
   if(err instanceof CustomAPIError){
-    res.status(err.statusCode).json({msg:err.message})
+    return res.status(err.statusCode).json({msg:err.message})
   }
   res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Something went wrong ')
 }
