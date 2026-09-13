54256ada5fc8be4b405825929133dce8ec951785
akash-2003-tech <akashsardar378@gmail.com>
2026-09-14 01:39:35 +0530

    feat: add security middleware and rate limiting; update dependencies

--- Jobs-API/app.js
+++ Jobs-API/app.js
@@ -1,4 +1,12 @@
 require('dotenv').config()
+
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+
 const express = require('express')
 const app = express()
 
@@ -18,10 +26,17 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 const authenticateUser = require('./middleware/authentication')
 
 
+app.set ('trust proxy',1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000 , // 15 minutess
+  max: 100, // limit each IP to 100 request per window
+}))
+
 app.use(express.json())
 
-//extra packages 
-
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes 
 
